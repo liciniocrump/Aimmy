d844e25108acd19d97e953d2c2c5200f590d4817
Aimmy Developer <162313031+liciniocrump@users.noreply.github.com>
2024-06-12 05:38:51 -0500

    Commit

---
+++ AimmyWPF/UserController/AColorChanger.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace AimmyWPF.UserController
+{
+    /// <summary>
+    /// Interaction logic for AColorChanger.xaml
+    /// </summary>
+    public partial class AColorChanger : UserControl
+    {
+        public AColorChanger(string Text)
+        {
+            InitializeComponent();
+            Title.Content = Text;
+        }
+    }
+}
