8680898aba8e9f9ca23038c4d02006dd0f2c843b
Aimmy Developer <162313031+liciniocrump@users.noreply.github.com>
2024-08-14 18:02:31 -0500

    Commit

---
+++ AimmyWPF/UserController/ALabel.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace AimmyWPF.UserController
+{
+    /// <summary>
+    /// Interaction logic for ALabel.xaml
+    /// </summary>
+    public partial class ALabel : UserControl
+    {
+        public ALabel(string Text)
+        {
+            InitializeComponent();
+            Title.Content = Text;
+        }
+    }
+}
